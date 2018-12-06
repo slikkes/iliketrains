280424a62728b9b5161bafaf808b2980fcd437f4
slikkistvan@gmail.com <slikkistvan@gmail.com>
2018-12-06 23:15:57 +0100

    show pos

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 let trains = [];
+let timer;
 
 function start(){
 	let velocity = [];
-	velocity[0] = parseInt($("#vel_1").val());
-	velocity[1] = parseInt($("#vel_2").val());
+	velocity[0] = parseInt($("#vel_1").val())/25;
+	velocity[1] = parseInt($("#vel_2").val())/25;
 
 	trains.push(new Train(velocity[0], "right", "train_1"))
 	trains.push(new Train(velocity[1], "left", "train_2"))
-	setInterval(update, 250);
+
+	clearInterval(timer);
+	timer = setInterval(update, 250);
 }
 
 function update(){
-	console.log($("#train_1").val())
 	trains.forEach(e => e.move());
+	checkPositions();
+}
 
+function checkPositions(){
+	if(trains[1].y < trains[0].y){
+		clearInterval(timer)
+		$("#pos").val(trains[0].y.toFixed(2))
+	}
 }
